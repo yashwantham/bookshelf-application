0954eba89bc2d55f00599a6a94687b5643cb35a5
Yashwanth <yashwanthyashu166@gmail.com>
2023-06-17 21:48:21 +0530

    feat: implemented search functionality

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,10 +1,33 @@
+import { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { BookContext } from "../../context/BooksProvider";
+import { BookCard } from "../HomePage/HomePageComponents/BookCard/BookCard";
+
+import "./SearchPage.css";
 
 export function SearchPage() {
+
+    const {bookdata} = useContext(BookContext);
+
+    // let displayBookdata = [...bookdata];
+    const [displayBookdata, setDisplayBookdata] = useState([...bookdata])
+
+    const inputChangeHandler = (e) => {
+        const searchResult = bookdata.filter(({title}) => title.toLowerCase().includes(e.target.value.toLowerCase()))
+        setDisplayBookdata(() => [...searchResult])
+    }
+
     return (
         <>
+        <div className="searchpage-container">
         <NavLink to="/">Go to library</NavLink>
-            <h1>Search Page</h1>
+        <div className="searchinput-container">
+            Search: <input type="text" onChange={inputChangeHandler}/>
+        </div>
+        <div className="booklist-container">
+            {displayBookdata.map((book) => <BookCard book={book}/>)}
+        </div>
+        </div>
         </>
     )
 }
